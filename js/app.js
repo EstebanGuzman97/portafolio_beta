084a7cdff6aeb42d819ac53f831970ecbe0a352d
EstebanGuzman97 <90728704+EstebanGuzman97@users.noreply.github.com>
2022-02-10 14:38:03 -0300

    Se agrega el proyecto

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function alertContacto(){
+    datosContacto = document.getElementById("datosContacto");
+
+    datosContacto.innerHTML = 
+    `<div class="card bg-indigo col" style="width: 18rem;">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Correo: esteban.guzman.1997@gmail.com</li>
+      <li class="list-group-item">Telefono: 11 5832 4909</li>
+    </ul>
+  </div>`
+}
